f462e3ab495576be40f58ec74ced989b09dd6f34
MohammadYehya <mohammad7446@gmail.com>
2025-02-05 12:13:52 +0500

    Added Projects Cards

--- src/app/Projects/page.tsx
+++ src/app/Projects/page.tsx
@@ -1,7 +1,27 @@
+import { Card } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import { data } from "@/lib/data/config";
+
 export default function Projects() {
   return (
-    <div className="my-10 flex w-full justify-center">
-      <h1 className="text-7xl font-bold">Projects</h1>
+    <div className="my-10 w-full flex justify-center">
+      <div className="w-[80%] flex justify-center flex-col">
+        <h1 className="text-7xl font-bold flex justify-center mb-5">
+          Projects
+        </h1>
+        <div className="flex flex-wrap justify-center gap-x-2">
+          {data.NavItems.Projects.content.map((item, index) => {
+            return (
+              <Card key={index} className="flex flex-1 flex-col min-w-[30%] max-w-[30%] p-5 ">
+                <div className="font-bold mb-4">{item.title}</div>
+                <Separator />
+                <div>{item.type}</div>
+                <div>{item.description}</div>
+              </Card>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
