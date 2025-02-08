d186ea2b3f6ee1a26c9aaac062d57de7735490a1
MohammadYehya <mohammad7446@gmail.com>
2025-02-09 04:06:31 +0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import Navbar from "@/components/myui/Navbar";
 
 import "./globals.css";
 
@@ -30,7 +29,6 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Navbar />
         {children}
       </body>
     </html>
