91d5ed1f88a3b0e60912f945c7ffa786cecded39
MohammadYehya <mohammad7446@gmail.com>
2025-02-09 04:07:01 +0500

    Update page.tsx

--- src/app/Resume/page.tsx
+++ src/app/Resume/page.tsx
@@ -1,7 +1,18 @@
+'use client'
+import { useEffect, useState } from "react";
+import Navbar from "@/components/myui/Navbar";
+
 export default function Resume() {
+  const [open, setOpen] = useState(false);
+      useEffect(() => {
+        setOpen(true);
+      },[]);
   return (
-    <div className="my-10 flex w-full justify-center">
-      <h1 className="text-7xl font-bold">Resume</h1>
-    </div>
+    <>
+      <Navbar />
+      <div className={`my-10 flex w-full justify-center transition-all duration-1000 ${open ? 'opacity-100' : 'opacity-0'}`}>
+        <h1 className="text-7xl font-bold">Resume</h1>
+      </div>
+    </>
   );
 }
