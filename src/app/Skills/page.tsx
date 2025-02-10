c876d63613b2ddd4bbc9bbbb8498e188aba028a9
MohammadYehya <mohammad7446@gmail.com>
2025-02-11 00:17:45 +0500

    Attempt to fix img buq

--- src/app/Skills/page.tsx
+++ src/app/Skills/page.tsx
@@ -1,18 +1,25 @@
-'use client'
+"use client";
 import { useEffect, useState } from "react";
 import Navbar from "@/components/myui/Navbar";
 import { Card } from "@/components/ui/card";
 import { MySkills } from "../../../public/data/config";
+import Image from "next/image";
+import dynamic from "next/dynamic";
 
 export default function Skills() {
   const [open, setOpen] = useState(false);
-    useEffect(() => {
-      setOpen(true);
-    },[]);
+  useEffect(() => {
+    MySkills.content.forEach((group) => {group.content.forEach((item) => {dynamic(() => import(item.imgpath))})})
+    setOpen(true);
+  }, []);
   return (
     <>
       <Navbar />
-      <div className={`my-10 w-full flex justify-center transition-all duration-1000 ${open ? 'opacity-100' : 'opacity-0'}`}>
+      <div
+        className={`my-10 w-full flex justify-center transition-all duration-1000 ${
+          open ? "opacity-100" : "opacity-0"
+        }`}
+      >
         <div className="flex px-4 md:w-[80%] justify-center flex-col">
           <h1 className="text-7xl font-bold flex justify-center mb-5">
             Skills
@@ -27,22 +34,31 @@ export default function Skills() {
                 <div className="border flex flex-1"></div>
               </div>
               <div className="flex flex-wrap justify-center gap-y-3 md:gap-x-3 mt-4">
-                {item.content.map((details, index) => (
-                  <Card
-                    key={index}
-                    className={`flex flex-row justify-start lg:flex-1 w-full md:min-w-[49%] md:max-w-[49%] lg:min-w-[30%] lg:max-w-[30%] p- h-14 hover:h-32 group items-center transition-all duration-300 ease-in-out overflow-clip`}
-                  >
-                    <div className="font-bold items-start flex w-full pl-5">
-                      {details.name}
-                    </div>
-                    <img src={details.imgpath} alt="" width={175} height={1} className="opacity-60" draggable={false}/>
-                    {/* <div
+                {item.content.map((details, index) => {
+                  return (
+                    <Card
+                      key={index}
+                      className={`flex flex-row justify-start lg:flex-1 w-full md:min-w-[49%] md:max-w-[49%] lg:min-w-[30%] lg:max-w-[30%] p- h-14 hover:h-32 group items-center transition-all duration-300 ease-in-out overflow-clip`}
+                    >
+                      <div className="font-bold items-start flex w-full pl-5">
+                        {details.name}
+                      </div>
+                      <Image
+                        src={details.imgpath}
+                        alt=""
+                        width={175}
+                        height={1}
+                        className="opacity-60"
+                        draggable={false}
+                      />
+                      {/* <div
                       className={`group-hover:flex text-transparent group-hover:text-black transition-all duration-500 ease-in-out`}
                     >
                       Proficiency: {details.proficiency}
                     </div> */}
-                  </Card>
-                ))}
+                    </Card>
+                  );
+                })}
               </div>
             </div>
           ))}
