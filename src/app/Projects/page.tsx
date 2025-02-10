6dca75c0edb11f5cc920a9ab32e05d89bd76652c
MohammadYehya <mohammad7446@gmail.com>
2025-02-10 23:17:00 +0500

    Refactor to add skill images

--- src/app/Projects/page.tsx
+++ src/app/Projects/page.tsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 import Navbar from "@/components/myui/Navbar";
 import { Card } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
-import { MyProjects } from "@/lib/data/config";
+import { MyProjects, MySkills } from "../../../public/data/config";
 import { CalendarClock, Component } from "lucide-react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
@@ -11,13 +11,24 @@ import { useRouter } from "next/navigation";
 export default function Projects() {
   const [open, setOpen] = useState(false);
   const router = useRouter();
-      useEffect(() => {
-        setOpen(true);
-      },[]);
+  useEffect(() => {
+    setOpen(true);
+  }, []);
+  function getPath(skill: string) {
+    return MySkills.content
+      .map(
+        (group) => group.content.find((item) => item.name === skill)?.imgpath
+      )
+      .toSorted()[0];
+  }
   return (
     <>
       <Navbar />
-      <div className={`my-10 w-full flex justify-center transition-all duration-1000 ${open ? 'opacity-100' : 'opacity-0'}`}>
+      <div
+        className={`my-10 w-full flex justify-center transition-all duration-1000 ${
+          open ? "opacity-100" : "opacity-0"
+        }`}
+      >
         <div className="flex px-4 md:px-14 justify-center flex-col">
           <h1 className="text-7xl font-bold flex justify-center mb-5">
             Projects
@@ -27,7 +38,7 @@ export default function Projects() {
               return (
                 <Card
                   key={index}
-                  className="flex md:flex-1 flex-col w-full md:min-w-[300px] md:max-w-[300px] p-5 hover:scale-110 hover:cursor-pointer transition-all duration-500"
+                  className="flex md:flex-1 flex-col w-full md:min-w-[300px] md:max-w-[300px] p-5 hover:scale-110 hover:cursor-pointer transition-all duration-500 shadow-2xl"
                   onClick={() => {
                     router.push(item.link);
                   }}
@@ -54,9 +65,16 @@ export default function Projects() {
                   <div className="flex flex-1 flex-wrap gap-2">
                     {item.tech.map((item, index) => (
                       <div
-                        className="w-10 h-10 border border-black rounded-md"
+                        className="w-10 h-10 border border-black rounded-md flex justify-center items-center"
                         key={index}
-                      ></div>
+                      >
+                        <Image
+                          src={`${getPath(item)}`}
+                          alt=""
+                          width={20}
+                          height={1}
+                        />
+                      </div>
                     ))}
                   </div>
                 </Card>
