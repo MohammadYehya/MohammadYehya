ab16022a2f2a1b62255ae6ec1fd4baa3d2a45839
MohammadYehya <mohammad7446@gmail.com>
2025-02-11 00:57:09 +0500

    Move code to different repo

--- src/components/myui/Transition.tsx
+++
@@ -1,5 +0,0 @@
-export function Transition ({route}:{route: boolean}) {
-    return (
-        <div className={`flex items-center justify-center transition-all duration-1000 rounded-full absolute bg-black h-screen w-screen ${route ? 'scale-150' : 'scale-0'} z-[200]`} />
-    )
-}
