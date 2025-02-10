b9c5b26083f683e645a5038bf8f6373fd62a250a
MohammadYehya <mohammad7446@gmail.com>
2025-02-10 23:18:19 +0500

    Created templates for Education adnd Experience

--- src/app/Experience/page.tsx
+++ src/app/Experience/page.tsx
@@ -1,35 +1,49 @@
-'use client'
+"use client";
 import { useEffect, useState } from "react";
 import Navbar from "@/components/myui/Navbar";
+import { Card } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import { CirclePlus } from "lucide-react";
+import { MyExperiences } from "../../../public/data/config";
 
 export default function Experience() {
   const [open, setOpen] = useState(false);
-      useEffect(() => {
-        setOpen(true);
-      },[]);
+  useEffect(() => {
+    setOpen(true);
+  }, []);
   return (
     <>
       <Navbar />
-      <div className={`my-10 flex flex-col w-full justify-center transition-all duration-1000 ${open ? 'opacity-100' : 'opacity-0'}`}>
-        <h1 className="text-7xl font-bold">Experience</h1>
-        {[1, 2, 3, 4].map((x, index) => (
-          <div
-            className={`flex ${
-              index % 2 !== 0 ? "flex-row-reverse" : "flex-row"
-            } gap-4`}
-            key={index}
-          >
-            <div className="flex flex-1 flex-col justify-center lg:py-[50px]">
-              {x}
+      <div className="w-full flex justify-center">
+        <div
+          className={`my-10 w-full lg:w-[70%] flex flex-col justify-center items-center transition-all duration-1000 lg:gap-y-0 gap-y-3 ${
+            open ? "opacity-100" : "opacity-0"
+          }`}
+        >
+          <h1 className="lg:text-7xl text-5xl font-bold mb-8">Experience</h1>
+          {MyExperiences.content.map((details, index) => (
+            <div
+              className="flex even:flex-row odd:flex-row-reverse w-full justify-center lg:p-0 p-2"
+              key={index}
+            >
+              <Card className="flex flex-1 h-96 shadow-2xl border-slate-300 p-4">
+                {}
+              </Card>
+              <div className="lg:flex lg:flex-col items-center mx-4 hidden ">
+                <Separator
+                  orientation="vertical"
+                  className="flex flex-1 bg-slate-400"
+                />
+                <CirclePlus />
+                <Separator
+                  orientation="vertical"
+                  className="flex flex-1 bg-slate-400"
+                />
+              </div>
+              <Card className="lg:flex lg:flex-1 opacity-0 hidden p-4"></Card>
             </div>
-            <div>
-              <div className="w-[1px] flex-1 bg-[--color]"></div>
-              <div className="my-2 text-[--icon]"></div>
-              <div className="w-[1px] flex-1 bg-[--color]"></div>
-            </div>
-            <div className="hidden min-w-0 flex-1 flex-shrink-0 flex-row lg:flex"></div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </>
   );
